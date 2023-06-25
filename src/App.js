e6f3961679175a3d392b5b43643e13d8f3ff0a3b
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2023-06-25 14:55:01 +0530

    added localStorage to store items in browser

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header/Header";
 import MainSection from "./components/MainSection/MainSection";
+// import Watchlist from "./Watchlist";
 function App() {
   return (
     <div className="App">
+      {/* <Watchlist /> */}
       <Header></Header>
       <MainSection></MainSection>
     </div>
